290709b3085e51860adc2a54cfade787c90cf9f8
ens-labs <danny@ens-labs.com>
2015-06-24 16:12:26 -0400

    Translated breakdownreport

--- breakdownreport.rb
+++ breakdownreport.rb
@@ -3,31 +3,31 @@ require 'savon'
 
 aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
 aee_client = Savon.client(wsdl: aee_url)
-breakdownSummary = aee_client.call(:get_breakdowns_summary)
+breakdowns_data = aee_client.call(:get_breakdowns_summary)
 
-hashtable = breakdownSummary.body
+breakdowns_summary = breakdowns_data.body
 
-hashtable.each do |key, value|
-	puts "Pueblos disponibles: "
-	for cada_pueblo in 0...value[:return].length
-		puts value[:return][cada_pueblo][:r1_town_or_city] + " " + value[:return][cada_pueblo][:r2_total_breakdowns]
+breakdowns_summary.each do |key, value|
+	puts "Available Towns: "
+	amount_of_towns = value[:return].length
+	for town in 0...amount_of_towns
+		puts value[:return][town][:r1_town_or_city] + " " + value[:return][town][:r2_total_breakdowns]
 	end
 end
 
-print "Pueblo: "
-pueblito = STDIN.gets.chomp()
-pueblito.upcase
+print "Town: "
+input_town = STDIN.gets.chomp()
 
-breakdownstuff = aee_client.call(:get_breakdowns_by_town_or_city, message: { "townOrCity" => pueblito })
+breakdowns_per_town = aee_client.call(:get_breakdowns_by_town_or_city, message: { "townOrCity" => input_town.upcase })
 
-hash_table = breakdownstuff.body
-hash_table.each do |key, value|
+hash_breakdonws_per_town = breakdowns_per_town.body
+hash_breakdonws_per_town.each do |key, value|
 	# Checks if it is an array of hashes containing all the averias specified to that town
 	# or a single hash with the specific breakdown for that pueblo
 	if value[:return].kind_of?(Array)
 		for averias in 0...value[:return].length
 			puts "***************************************"
-			puts "Pueblo: " + value[:return][averias][:r1_town_or_city]
+			puts "Town: " + value[:return][averias][:r1_town_or_city]
 			puts "Area: " + value[:return][averias][:r2_area]
 			puts "Status: " + value[:return][averias][:r3_status]
 			puts "Last Update: " + value[:return][averias][:r4_last_update]
@@ -35,7 +35,7 @@ hash_table.each do |key, value|
 		end
 	else
 		puts "***************************************"
-		puts "Pueblo: " + value[:return][:r1_town_or_city]
+		puts "Town: " + value[:return][:r1_town_or_city]
 		puts "Area: " + value[:return][:r2_area]
 		puts "Status: " + value[:return][:r3_status]
 		puts "Last Update: " + value[:return][:r4_last_update]

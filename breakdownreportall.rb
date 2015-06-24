18da819720a49d7657ff2de7d1fd1005494e2756
ens-labs <danny@ens-labs.com>
2015-06-24 16:18:45 -0400

    translated reportall

--- breakdownreportall.rb
+++ breakdownreportall.rb
@@ -2,36 +2,37 @@
 require 'savon'
 require 'colorize'
 aee_url = 'http://wss.prepa.com/services/BreakdownReport?wsdl'
-pueblos = Array.new
+towns = Array.new
 
 aee_client = Savon.client(wsdl: aee_url)
-breakdownSummary = aee_client.call(:get_breakdowns_summary)
-hashtable = breakdownSummary.body
+breakdowns_data = aee_client.call(:get_breakdowns_summary)
+breakdowns_summary = breakdowns_data.body
 
-hashtable.each do |key, value|
-	for cada_pueblo in 0...value[:return].length
-		pueblos.push value[:return][cada_pueblo][:r1_town_or_city]
+breakdowns_summary.each do |key, value|
+	amount_of_towns = valuep[:return].length
+	for cada_pueblo in 0...amount_of_towns
+		towns.push value[:return][cada_pueblo][:r1_town_or_city]
 	end
 end
 
-pueblos.each do |value|
-	breakdownstuff = aee_client.call(:get_breakdowns_by_town_or_city, message: { "townOrCity" => value })
-	data = breakdownstuff.body
-	if data[:get_breakdowns_by_town_or_city_response][:return].kind_of?(Array)
-		for averias in 0...data[:get_breakdowns_by_town_or_city_response][:return].length
+towns.each do |value|
+	breakdowns_per_town = aee_client.call(:get_breakdowns_by_town_or_city, message: { "townOrCity" => value })
+	total_breakdowns = breakdowns_per_town.body
+	if total_breakdowns[:get_breakdowns_by_town_or_city_response][:return].kind_of?(Array)
+		for breakdowns in 0...total_breakdowns[:get_breakdowns_by_town_or_city_response][:return].length
 			puts "***************************************"
-			puts "Pueblo: " + data[:get_breakdowns_by_town_or_city_response][:return][averias][:r1_town_or_city]
-			puts "Area: " + data[:get_breakdowns_by_town_or_city_response][:return][averias][:r2_area]
-			puts "Status: " + data[:get_breakdowns_by_town_or_city_response][:return][averias][:r3_status]
-			puts "Last Update: " + data[:get_breakdowns_by_town_or_city_response][:return][averias][:r4_last_update]
+			puts "Pueblo: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r1_town_or_city]
+			puts "Area: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r2_area]
+			puts "Status: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r3_status]
+			puts "Last Update: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r4_last_update]
 			puts "***************************************"
 		end
 	else
 		puts "***************************************"
-		puts "Pueblo: " + data[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]
-		puts "Area: " + data[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
-		puts "Status: " + data[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
-		puts "Last Update: " + data[:get_breakdowns_by_town_or_city_response][:return][:r4_last_update]
+		puts "Pueblo: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]
+		puts "Area: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
+		puts "Status: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
+		puts "Last Update: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r4_last_update]
 		puts "***************************************"
 	end
 end

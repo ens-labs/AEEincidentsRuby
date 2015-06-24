f4a21f9d9f2dc677de0a972941b55d666444f9ac
ens-labs <danny@ens-labs.com>
2015-06-24 16:19:12 -0400

    fixed typo

--- breakdownreportall.rb
+++ breakdownreportall.rb
@@ -9,7 +9,7 @@ breakdowns_data = aee_client.call(:get_breakdowns_summary)
 breakdowns_summary = breakdowns_data.body
 
 breakdowns_summary.each do |key, value|
-	amount_of_towns = valuep[:return].length
+	amount_of_towns = value[:return].length
 	for cada_pueblo in 0...amount_of_towns
 		towns.push value[:return][cada_pueblo][:r1_town_or_city]
 	end
@@ -21,7 +21,7 @@ towns.each do |value|
 	if total_breakdowns[:get_breakdowns_by_town_or_city_response][:return].kind_of?(Array)
 		for breakdowns in 0...total_breakdowns[:get_breakdowns_by_town_or_city_response][:return].length
 			puts "***************************************"
-			puts "Pueblo: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r1_town_or_city]
+			puts "Town: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r1_town_or_city]
 			puts "Area: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r2_area]
 			puts "Status: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r3_status]
 			puts "Last Update: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][breakdowns][:r4_last_update]
@@ -29,7 +29,7 @@ towns.each do |value|
 		end
 	else
 		puts "***************************************"
-		puts "Pueblo: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]
+		puts "Town: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r1_town_or_city]
 		puts "Area: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r2_area]
 		puts "Status: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r3_status]
 		puts "Last Update: " + total_breakdowns[:get_breakdowns_by_town_or_city_response][:return][:r4_last_update]
